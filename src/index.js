b23bb684f34309179191cc1fe7f2a9838594f832
AshisLive <ashu.kumar@gmail.com>
2023-02-26 06:17:47 +0530

    mongoDB connection update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const midglobal = function (req, res, next) {
 }
 app.use(midglobal)
 
-mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/RaviKantKannaujiya123_db?authSource=admin&replicaSet=atlas-60843q-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/ASHUTOSH_KUMAR_BGS-DB?retryWrites=true&w=majority", {useNewUrlParser: true})
     .then(() => console.log('mongodb running on 27017'))
     .catch(err => console.log(err))
 
